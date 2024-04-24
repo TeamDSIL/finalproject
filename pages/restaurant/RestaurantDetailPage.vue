b0e47228d8ff5fbd7e157856f9aa550e8dc9fbc0
MSH0512 <wndqhr00@gmail.com>
2024-04-24 17:29:38 +0900

    #24 식당 리스트, 식당 상세조회 프론트 작업

---
+++ pages/restaurant/RestaurantDetailPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 >식당 상세조회</h1>
+    <restaurantDetail></restaurantdetail>
+    </div>
+  
+</template>
+
+<script>
+import RestaurantDetail from '@/components/restaurant/RestaurantDetail.vue';
+
+export default {
+  components: {
+    RestaurantDetail,
+  },
+};
+</script>
+
+<style></style>
