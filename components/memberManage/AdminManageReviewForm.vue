c3279342014ef4d465aba5cf10cdc5b5cb2c6d77
Hwije-Jung <gnlwp96@naver.com>
2024-05-13 15:21:53 +0900

    #101 feat: 총관리자 삭제요청 리뷰 삭제 구현

--- components/memberManage/AdminManageReviewForm.vue
+++ components/memberManage/AdminManageReviewForm.vue
@@ -1,244 +1,247 @@
 <template>
-    <div>
-        <v-container>
+  <div>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <Box class="dashboard-box">
+            <div slot="boxSidebar">
+              <AdminDashBoardSideBar />
+            </div>
+            <div slot="boxContent">
+              <div class="px-10">
+                <v-row>
+                  <v-col cols="12">
+                    <h1>리뷰 및 댓글 삭제 관리</h1>
+                  </v-col>
 
+                  <v-col cols="12">
+                    <div>
+                      <v-container>
+                        <!-- 검색 필드 -->
+                        <v-text-field
+                          v-model="searchQuery"
+                          label="검색어를 입력하세요"
+                          outlined
+                          dense
+                          clearable
+                          append-icon="mdi-magnify"
+                          @keyup.enter="performSearch"
+                        ></v-text-field>
 
-            <v-row>
-                <v-col cols="12">
-                    <Box class="dashboard-box">
-                        <div slot="boxSidebar">
-                            <AdminDashBoardSideBar />
+                        <!-- 회원 정보 목록 테이블 -->
+                        <v-data-table
+                          :headers="headers"
+                          :items="displayedMembers"
+                          hide-default-footer
+                        >
+                          <template v-slot:item="{ item }">
+                            <tr>
+                              <td>{{ item.reviewId }}</td>
+                              <td>{{ item.userEamil }}</td>
+                              <td>{{ truncateString(item.replyContent, 13) }}</td>
+                              <td>{{ truncateString(item.reviewContent, 13) }}</td>
+                              <td>
+                                <v-dialog
+                                  max-width="500"
+                                  :class="{ 'overlay-hidden': !showDialog }"
+                                >
+                                  <template v-slot:activator="{ on }">
+                                    <v-btn
+                                      color="primary"
+                                      @click="openReviewInfoForm(item)"
+                                      v-on="on"
+                                      >상세정보</v-btn
+                                    >
+                                  </template>
+                                  <ReviewInfoForm
+                                    v-if="showDialog"
+                                    :reviewInfo="reviewInfo"
+                                    @close-dialog="handleCloseDialog"
+                                  />
+                                </v-dialog>
+                              </td>
+                            </tr>
+                          </template>
+                        </v-data-table>
+                      </v-container>
+                    </div>
+                  </v-col>
 
-                        </div>
-                        <div slot="boxContent">
-                            <div class="px-10">
-                                <v-row>
-                                    <v-col cols="12">
-                                        <h1>리뷰 및 댓글 삭제 관리</h1>
-                                    </v-col>
-
-                                    <v-col cols="12">
-                                        <div>
-                                            <v-container>
-                                                <!-- 검색 필드 -->
-                                                <v-text-field v-model="searchQuery" label="검색어를 입력하세요" outlined dense
-                                                    clearable append-icon="mdi-magnify"
-                                                    @keyup.enter="performSearch"></v-text-field>
-
-                                                <!-- 회원 정보 목록 테이블 -->
-                                                <v-data-table :headers="headers" :items="displayedMembers"
-                                                    hide-default-footer>
-                                                    <template v-slot:item="{ item }">
-                                                        <tr>
-                                                            <td>{{ item.id }}</td>
-                                                            <td>{{ item.email }}</td>
-                                                            <td>{{ item.reply }}</td>
-                                                            <td>{{ item.review }}</td>
-                                                            <td>
-                                                                <v-dialog max-width="500">
-                                                                    <template v-slot:activator="{ on }">
-                                                                        <v-btn color="primary"
-                                                                            @click="openReviewInfoForm(item)" v-on="on"
-                                                                            :disabled="!item">상세정보</v-btn>
-                                                                    </template>
-
-                                                                    <ReviewInfoForm v-if="showDialog"
-                                                                        :reviewInfo="reviewInfo" />
-                                                                </v-dialog>
-                                                            </td>
-                                                        </tr>
-                                                    </template>
-                                                </v-data-table>
-                                            </v-container>
-                                        </div>
-
-                                    </v-col>
-
-                                    <v-col cols="12">
-                                        <div
-                                            class="d-flex justify-center align-center justify-sm-space-between flex-wrap">
-                                            <div class="mb-4 me-3">
-                                                <p class="font-weight-normal mb-0 text-14">Showing {{ startItemIndex
-                                                    }}-{{ endItemIndex }} of {{
-                                                        filteredMembers.length }} Reviews</p>
-                                            </div>
-                                            <div class="mb-4">
-                                                <!-- 페이지네이션 -->
-                                                <v-pagination v-model="currentPage" :length="numberOfPages" circle
-                                                    @input="navigateToPage"></v-pagination>
-
-
-                                            </div>
-                                        </div>
-                                    </v-col>
-
-
-
-
-
-
-                                </v-row>
-                            </div>
-                        </div>
-                    </Box>
-                </v-col>
-            </v-row>
-        </v-container>
-        <Footer />
-
-    </div>
+                  <v-col cols="12">
+                    <div
+                      class="d-flex justify-center align-center justify-sm-space-between flex-wrap"
+                    >
+                      <div class="mb-4 me-3">
+                        <p class="font-weight-normal mb-0 text-14">
+                          Showing {{ startItemIndex }}-{{ endItemIndex }} of
+                          {{ filteredMembers.length }} Reviews
+                        </p>
+                      </div>
+                      <div class="mb-4">
+                        <!-- 페이지네이션 -->
+                        <v-pagination
+                          v-model="currentPage"
+                          :length="numberOfPages"
+                          circle
+                          @input="navigateToPage"
+                        ></v-pagination>
+                      </div>
+                    </div>
+                  </v-col>
+                </v-row>
+              </div>
+            </div>
+          </Box>
+        </v-col>
+      </v-row>
+    </v-container>
+    <Footer />
+  </div>
 </template>
 <script>
-
-import AdminModifyUserForm from '@/components/memberManage/AdminModifyUserForm.vue';
-import ReviewInfoForm from '@/components/memberManage/ReviewInfoForm.vue';
+import AdminModifyUserForm from "@/components/memberManage/AdminModifyUserForm.vue";
+import ReviewInfoForm from "@/components/memberManage/ReviewInfoForm.vue";
+import axios from "axios";
 
 export default {
-    head: {
-        title: 'Order List'
+  head: {
+    title: "Order List",
+  },
+  components: {
+    AdminModifyUserForm,
+    ReviewInfoForm,
+  },
+  data() {
+    return {
+      page: 1,
+      step: 1, // 초기 단계 설정
+      searchQuery: "",
+      currentPage: 1, // 현재 페이지 번호
+      itemsPerPage: 10, // 페이지당 표시할 아이템 수
+      showDialog: false,
+      reviewInfo: {}, // 리뷰 정보를 저장할 객체 추가
+      reviewInfos: [],
+      reviewsList: [],
+      headers: [
+        { text: "No", align: "start", value: "id" },
+        { text: "이메일", value: "email" },
+        { text: "댓글", value: "reply" },
+        { text: "리뷰", value: "review" },
+        { text: "상세보기", value: "" },
+      ],
+    };
+  },
+  created() {
+    this.fetchReviews();
+  },
+  computed: {
+    // 검색어를 기반으로 필터링된 회원 정보 반환
+    filteredMembers() {
+      return this.reviewInfos.filter(
+        (review) =>
+          review.userEamil.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          review.replyContent.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+          review.reviewContent.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
     },
-    components: {
-        AdminModifyUserForm,
-        ReviewInfoForm
+    displayedMembers() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = this.currentPage * this.itemsPerPage;
+      return this.filteredMembers.slice(startIndex, endIndex);
     },
-    data() {
-        return {
-            page: 1,
-            step: 1, // 초기 단계 설정
-            searchQuery: '',
-            currentPage: 1, // 현재 페이지 번호
-            itemsPerPage: 10, // 페이지당 표시할 아이템 수
-            showDialog: false,
-            reviewInfo: {}, // 리뷰 정보를 저장할 객체 추가
-            reviewInfos: [
-                { id: 1, email: 'example1@mail.com', reply: 'John Doe', review: '+1 123-456-7890' },
-                { id: 2, email: 'example2@mail.com', reply: 'Jane Doe', review: '+1 987-654-3210' },
-                { id: 3, email: 'example3@mail.com', reply: 'Emily Smith', review: '+1 555-123-4567' },
-                { id: 4, email: 'example4@mail.com', reply: 'Michael Johnson', review: '+1 555-987-6543' },
-                { id: 5, email: 'example5@mail.com', reply: 'Sophia Williams', review: '+1 555-456-7890' },
-                { id: 6, email: 'example6@mail.com', reply: 'James Brown', review: '+1 555-876-5432' },
-                { id: 7, email: 'example7@mail.com', reply: 'Olivia Davis', review: '+1 555-234-5678' },
-                { id: 8, email: 'example8@mail.com', reply: 'William Miller', review: '+1 555-678-9012' },
-                { id: 9, email: 'example9@mail.com', reply: 'Emma Wilson', review: '+1 555-345-6789' },
-                { id: 10, email: 'example10@mail.com', reply: 'Alexander Moore', review: '+1 555-789-0123' },
-                { id: 11, email: 'example11@mail.com', reply: 'Isabella Taylor', review: '+1 555-456-7890' },
-                { id: 12, email: 'example12@mail.com', reply: 'Ethan Anderson', review: '+1 555-890-1234' },
-                { id: 13, email: 'example13@mail.com', reply: 'Mia Thomas', review: '+1 555-567-8901' },
-                { id: 14, email: 'example14@mail.com', reply: 'Daniel Jackson', review: '+1 555-012-3456' },
-                { id: 15, email: 'example15@mail.com', reply: 'Ava White', review: '+1 555-678-9012' },
-                { id: 16, email: 'example16@mail.com', reply: 'Matthew Harris', review: '+1 555-234-5678' },
-                { id: 17, email: 'example17@mail.com', reply: 'Chloe Martin', review: '+1 555-901-2345' },
-                { id: 18, email: 'example18@mail.com', reply: 'Charlotte Thompson', review: '+1 555-345-6789' },
-                { id: 19, email: 'example19@mail.com', reply: 'Liam Garcia', review: '+1 555-678-9012' },
-                { id: 20, email: 'example20@mail.com', reply: 'Amelia Martinez', review: '+1 555-012-3456' },
-                { id: 21, email: 'example21@mail.com', reply: 'Benjamin Robinson', review: '+1 555-234-5678' },
-                { id: 22, email: 'example22@mail.com', reply: 'Harper Clark', review: '+1 555-345-6789' },
-                { id: 23, email: 'example23@mail.com', reply: 'Evelyn Hall', review: '+1 555-456-7890' },
-                { id: 24, email: 'example24@mail.com', reply: 'Lucas Lewis', review: '+1 555-567-8901' },
-                { id: 25, email: 'example25@mail.com', reply: 'Aiden Lee', review: '+1 555-678-9012' },
-            ],
-            headers: [
-                { text: 'No', align: 'start', value: 'id' },
-                { text: '이메일', value: 'email' },
-                { text: '댓글', value: 'reply' },
-                { text: '리뷰', value: 'review' },
-                { text: '상세보기', value: '' },
-            ],
-        }
+    startItemIndex() {
+      return (this.currentPage - 1) * this.itemsPerPage + 1;
     },
-    computed: {
-        // 검색어를 기반으로 필터링된 회원 정보 반환
-        filteredMembers() {
-            return this.reviewInfos.filter(review =>
-                review.email.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-                review.reply.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-                review.review.toLowerCase().includes(this.searchQuery.toLowerCase())
-            );
-        },
-        displayedMembers() {
-            const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-            const endIndex = this.currentPage * this.itemsPerPage;
-            return this.filteredMembers.slice(startIndex, endIndex);
-        },
-        startItemIndex() {
-            return (this.currentPage - 1) * this.itemsPerPage + 1;
-        },
-        // 현재 페이지에 해당하는 아이템의 끝 인덱스
-        endItemIndex() {
-            const endIndex = this.currentPage * this.itemsPerPage;
-            return endIndex > this.filteredMembers.length ? this.filteredMembers.length : endIndex;
-        },
-        // 필터링된 회원 수에 따른 페이지 수
-        numberOfPages() {
-            return Math.ceil(this.filteredMembers.length / this.itemsPerPage);
-        },
-        paginationOptions() {
-            return {
-                currentPage: this.currentPage,
-                itemsPerPage: this.itemsPerPage,
-                // 다른 페이지네이션 옵션들도 필요한 경우 추가할 수 있습니다.
-            };
-
-        },
+    // 현재 페이지에 해당하는 아이템의 끝 인덱스
+    endItemIndex() {
+      const endIndex = this.currentPage * this.itemsPerPage;
+      return endIndex > this.filteredMembers.length
+        ? this.filteredMembers.length
+        : endIndex;
     },
-    methods: {
-        openReviewInfoForm(reviewInfo) {
-            // 선택된 사용자의 모든 정보 저장
-            this.reviewInfo = {
-                id: reviewInfo.id,
-                email: reviewInfo.email,
-                reply: reviewInfo.reply,
-                review: reviewInfo.review
-            };
-            // 다이얼로그 표시
-            this.showDialog = true;
-        },
-        navigateToPage(newPage) {
-            this.currentPage = newPage;
-            // 페이지가 변경될 때마다 현재 페이지에 해당하는 회원 목록을 설정합니다.
-            const startIndex = (newPage - 1) * this.itemsPerPage;
-            const endIndex = newPage * this.itemsPerPage;
-            this.displayedMembers = this.filteredMembers.slice(startIndex, endIndex);
-        },
-        displayUserInfo() {
-            // 임시 비밀번호 전송 로직 처리
-            console.log('일반 회원 정보 관리');
-            this.step = 1; // 다음 단계로 이동
-        },
-        displayOwnerInfo() {
-            // 임시 비밀번호 인증 로직 처리
-            console.log('식당 관리자 정보 관리');
-            this.step = 2; // 다음 단계로 이동
-        },
-        performSearch() {
-            // 여기에 검색 로직을 구현합니다
-            console.log('검색어:', this.searchQuery);
-            // 실제 검색 로직을 수행하는 부분을 작성하세요
-        },
-        openDetailDialog(item) {
-            this.selectedMember = item;
-            this.dialogVisible = true; // 다이얼로그를 열기 위해 dialogVisible 값을 true로 설정
-        },
-        // 검색, 정렬 및 페이지 변경 시 실행되는 이벤트 핸들러
-        updateFilteredMembers(newMembers) {
-            this.filteredMembers = newMembers;
-        },
+    // 필터링된 회원 수에 따른 페이지 수
+    numberOfPages() {
+      return Math.ceil(this.filteredMembers.length / this.itemsPerPage);
     },
+    paginationOptions() {
+      return {
+        currentPage: this.currentPage,
+        itemsPerPage: this.itemsPerPage,
+        // 다른 페이지네이션 옵션들도 필요한 경우 추가할 수 있습니다.
+      };
+    },
+  },
+  methods: {
+    fetchReviews() {
+      axios
+        .get(`http://localhost:8000/memberManage/reviewManage`)
+        .then((response) => {
+          this.reviewInfos = response.data;
+          console.log(this.reviewInfos);
+        })
+        .catch((error) => {
+          console.error("데이터를 불러오는 중 오류가 발생했습니다:", error);
+        });
+    },
+    truncateString(str, num) {
+  if (!str) {
+    // 입력 문자열이 비어있는 경우
+    return "댓글이 없습니다.";
+  } else if (str.length > num) {
+    // 입력 문자열이 지정된 길이보다 길 경우
+    return str.slice(0, num) + "...";
+  } else {
+    // 입력 문자열이 지정된 길이 이내인 경우
+    return str;
+  }
 }
-
+,
+    openReviewInfoForm(reviewInfo) {
+      // 선택된 사용자의 모든 정보 저장
+        this.reviewInfo = {
+            userEamil: reviewInfo.userEamil,
+            userId: reviewInfo.userId,
+            reviewContent: reviewInfo.reviewContent,
+            reviewId: reviewInfo.reviewId,
+            reviewStatus: reviewInfo.reviewStatus,
+            registerDate: reviewInfo.registerDate,
+            replyContent: reviewInfo.replyContent,
+            replyId: reviewInfo.replyId,
+            replyStatus:reviewInfo.replyStatus,
+      };
+      // 다이얼로그 표시
+      this.showDialog = true;
+    },
+    navigateToPage(newPage) {
+      this.currentPage = newPage;
+      // 페이지가 변경될 때마다 현재 페이지에 해당하는 회원 목록을 설정합니다.
+      const startIndex = (newPage - 1) * this.itemsPerPage;
+      const endIndex = newPage * this.itemsPerPage;
+      this.displayedMembers = this.filteredMembers.slice(startIndex, endIndex);
+    },
+    openDetailDialog(item) {
+      this.selectedMember = item;
+      this.dialogVisible = true; // 다이얼로그를 열기 위해 dialogVisible 값을 true로 설정
+    },
+    // 검색, 정렬 및 페이지 변경 시 실행되는 이벤트 핸들러
+    updateFilteredMembers(newMembers) {
+      this.filteredMembers = newMembers;
+    },
+  },
+};
 </script>
 <style lang="scss">
 #select-user-owner,
 #direction-btn {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
 }
 
 #text-nowrap {
-    white-space: nowrap;
+  white-space: nowrap;
 }
 
 .search-container {
-    margin-top: 20px;
+  margin-top: 20px;
 }
 </style>
--- components/memberManage/ReviewInfoForm.vue
+++ components/memberManage/ReviewInfoForm.vue
@@ -1,73 +1,122 @@
 <template>
-    <v-card class="pa-4">
-        <h1>리뷰 및 댓글 삭제 관리</h1>
-        <div class="d-flex align-center mt-3">
-            <v-icon class="mr-2">mdi-calendar</v-icon>
-            <span class="font-weight-bold mr-2">등록날짜:</span>
-            <span>{{ reviewInfo.registDate }}</span>
-        </div>
-        <v-divider></v-divider>
-        <div class="d-flex align-center">
-            <v-icon class="mr-2">mdi-email</v-icon>
-            <span class="font-weight-bold mr-2">이메일:</span>
-            <span>{{ reviewInfo.email }}</span>
-        </div>
-        <v-divider></v-divider>
-        <div class="d-flex mt-3" style="justify-content: flex-start;">
-            <v-icon class="mr-2 align-baseline">mdi-message-text</v-icon>
-            <span class="font-weight-bold mr-2">리뷰:</span>
-            <v-text-field v-model="reviewInfo.review" outlined readonly></v-text-field>
-        </div>
-        <v-divider></v-divider>
-        <div class="d-flex mt-3" style="justify-content: flex-start;">
-            <v-icon class="mr-2 align-baseline">mdi-comment</v-icon>
-            <span class="font-weight-bold mr-2">댓글:</span>
-            <v-text-field v-model="reviewInfo.reply" outlined readonly></v-text-field>
-        </div>
-
-        <div class="mt-5 d-flex justify-end">
-            <v-btn color="secondary" class="mr-4" @click="deleteReview">리뷰 삭제</v-btn>
-            <v-btn color="error" class="mr-4" @click="deleteComment">댓글 삭제</v-btn>
-            <v-btn color="success" class="mr-4" @click="processRequest">요청 처리</v-btn>
-            <v-btn color="warning" @click="closeDialog">닫기</v-btn>
-        </div>
-
-
-
-    </v-card>
+  <v-card class="pa-4">
+    <h1>리뷰 및 댓글 삭제 관리</h1>
+    <div class="d-flex align-center mt-3">
+      <v-icon class="mr-2">mdi-calendar</v-icon>
+      <span class="font-weight-bold mr-2">등록날짜:</span>
+      <span>{{ reviewInfo.registerDate }}</span>
+    </div>
+    <v-divider></v-divider>
+    <div class="d-flex align-center">
+      <v-icon class="mr-2">mdi-email</v-icon>
+      <span class="font-weight-bold mr-2">이메일:</span>
+      <span>{{ reviewInfo.userEamil }}</span>
+    </div>
+    <v-divider></v-divider>
+    <div class="d-flex mt-3" style="justify-content: flex-start">
+      <v-icon class="mr-2 align-baseline">mdi-message-text</v-icon>
+      <span class="font-weight-bold mr-2">리뷰:</span>
+      <v-textarea
+        v-model="reviewInfo.reviewContent"
+        outlined
+        readonly
+        auto-grow
+        rows="1"
+      ></v-textarea>
+    </div>
+    <v-divider></v-divider>
+    <div class="d-flex mt-3" style="justify-content: flex-start">
+      <v-icon class="mr-2 align-baseline">mdi-comment</v-icon>
+      <span class="font-weight-bold mr-2">댓글:</span>
+      <!-- placeholder를 사용하여 "댓글이 없습니다." 표시 -->
+      <v-textarea
+        v-model="reviewInfo.replyContent"
+        :placeholder="reviewInfo.replyContent ? '' : '댓글이 없습니다.'"
+        outlined
+        readonly
+        auto-grow
+        rows="1"
+      ></v-textarea>
+    </div>
+    <div class="mt-5 d-flex justify-end">
+      <v-btn color="secondary" class="mr-4" @click="removeReview(reviewInfo.reviewId)"
+        >리뷰 삭제</v-btn
+      >
+      <v-btn
+        color="error"
+        class="mr-4"
+        @click="removeReply(reviewInfo.replyId)"
+        >댓글 삭제</v-btn
+      >
+      <!-- <v-btn color="success" class="mr-4" @click="processRequest">요청 처리</v-btn> -->
+      <!-- <v-btn color="warning" @click="closeDialog">닫기</v-btn> -->
+    </div>
+  </v-card>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    props: {
-        reviewInfo: {
-            type: Object,
-            required: true
-        },
+  props: {
+    // reviewInfo: Object,
+    reviewInfo: {
+      type: Object,
+      required: true,
     },
-    data() {
-        return {
-            showDialog: false // 다이얼로그의 표시 여부를 나타내는 데이터 속성 추가
-        };
+  },
+  data() {
+    return {
+      // showDialog: false // 다이얼로그의 표시 여부를 나타내는 데이터 속성 추가
+    };
+  },
+  methods: {
+    async removeReview(reviewId) {
+          try {
+        console.log(reviewId+"삭제할 리뷰아이디");
+        const response = await axios.delete(
+          `http://localhost:8000/memberManage/removeReview`,
+          {
+            data: {
+              reviewId: reviewId, // bookmarkId를 reviewId로 전달
+            },
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        alert("댓글이 삭제되었습니다.");
+
+        location.reload();
+      } catch (error) {
+        console.error("댓글 삭제 중 오류가 발생했습니다.", error);
+        alert("댓글 삭제에 실패했습니다.");
+      }
+      },
+      async removeReply(reviewId) {
+          try {
+        console.log(reviewId+"삭제할 댓글아이디");
+        const response = await axios.delete(
+          `http://localhost:8000/memberManage/removeReply`,
+          {
+            data: {
+                reviewId: reviewId, // bookmarkId를 reviewId로 전달
+            },
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        alert("댓글이 삭제되었습니다.");
+
+        location.reload();
+      } catch (error) {
+        console.error("댓글 삭제 중 오류가 발생했습니다.", error);
+        alert("댓글 삭제에 실패했습니다.");
+      }
     },
-    methods: {
-        deleteReview() {
-            // 리뷰 삭제 로직 추가
-        },
-        deleteComment() {
-            // 댓글 삭제 로직 추가
-        },
-        processRequest() {
-            // 요청 처리 로직을 여기에 추가
-        },
-        closeDialog() {
-            this.showDialog = false; // 다이얼로그를 닫기 위해 showDialog 값을 false로 설정합니다.
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
-
+<style></style>
