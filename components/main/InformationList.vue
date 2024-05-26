09c749b160fa5aa5255f4c6a5c39199bdd9232d8
LEEDAHYE0513 <ssgdlekgp6801@gmail.com>
2024-05-26 14:40:26 +0900

    #193 Refactor: 공지사항 생성, 수정, 삭제 기능 삭제

--- components/main/InformationList.vue
+++ components/main/InformationList.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="12">
         <div style="display: flex">
-          <v-img
+          <!-- <v-img
             contain
             :src="require('~/assets/images/board.png')"
             width="20px"
@@ -11,14 +11,8 @@
           ></v-img>
           <div style="font-size: larger; font-weight: bold">
             {{ InformationName }} 게시판
-          </div>
+          </div> -->
         </div>
-        <v-btn
-          @click="openModal()"
-          style="color: rgb(210, 63, 87); margin-top: 15px"
-        >
-          <v-icon>mdi-plus</v-icon> 새 공지 작성
-        </v-btn>
       </v-col>
     </v-row>
     <v-row>
@@ -49,113 +43,18 @@
               <td>{{ item.category }}</td>
               <td>{{ item.title }}</td>
               <td>{{ item.postDate }}</td>
-              <td>
-                <v-btn icon color="blue" @click.stop="editNotice(item)">
-                  <v-icon>mdi-pencil</v-icon>
-                </v-btn>
-                <v-btn icon color="red" @click.stop="deleteNotice(item.id)">
-                  <v-icon>mdi-delete</v-icon>
-                </v-btn>
-              </td>
             </tr>
           </template>
         </v-data-table>
       </v-col>
     </v-row>
-    <Footer />
-
-    <!-- Create and Edit Notice Modal -->
-    <v-dialog v-model="showCreateNoticeModal" max-width="600px">
-      <v-card
-        class="pa-4"
-        style="border-radius: 12px; box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1)"
-      >
-        <v-card-title
-          class="headline"
-          :class="editingNotice ? 'notice-edit' : 'notice-create'"
-          style="
-            font-weight: bold;
-            margin-left: 10px;
-            font-size: larger;
-            color: #333;
-          "
-        >
-          {{ editingNotice ? "공지사항 수정" : "공지사항 작성" }}
-        </v-card-title>
-        <v-card-text>
-          <v-container>
-            <v-row>
-              <v-col cols="12">
-                <v-select
-                  v-model="currentNotice.category"
-                  :items="noticeCategorys"
-                  label="공지사항 유형"
-                  required
-                  outlined
-                  dense
-                ></v-select>
-              </v-col>
-              <v-col cols="12">
-                <v-text-field
-                  v-model="currentNotice.title"
-                  label="공지 제목"
-                  required
-                  outlined
-                  dense
-                ></v-text-field>
-              </v-col>
-              <v-col cols="12">
-                <v-textarea
-                  v-model="currentNotice.contents"
-                  label="내용"
-                  required
-                  outlined
-                  rows="6"
-                  dense
-                ></v-textarea>
-              </v-col>
-              <v-col cols="12">
-                <v-file-input
-                  v-model="currentNotice.filePath"
-                  label="파일 첨부"
-                  prepend-icon="mdi-paperclip"
-                  accept="image/*, application/pdf"
-                  @change="onFileChanged"
-                  outlined
-                  dense
-                ></v-file-input>
-                <!-- 이미지 미리보기 -->
-                <v-img
-                  v-if="imageUrl"
-                  :src="imageUrl"
-                  height="200"
-                  class="mt-4"
-                  style="border-radius: 8px; object-fit: cover"
-                ></v-img>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="red" text @click="closeModal">취소</v-btn>
-          <v-btn
-            class="register-btn"
-            :class="editingNotice ? 'update-btn' : 'create-btn'"
-            @click="editingNotice ? updateNotice() : createNotice()"
-            style="background-color: rgb(210, 63, 87); color: white"
-            >등록</v-btn
-          >
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
 
     <!-- Details Modal -->
     <v-dialog v-model="showDetailsModal" max-width="600px">
       <v-card style="padding-top: 10px">
-        <v-card-title style="font-weight: bold; margin-left: 10px">
-          공지사항 상세 조회
-        </v-card-title>
+        <v-card-title style="font-weight: bold; margin-left: 10px"
+          >공지사항 상세 조회</v-card-title
+        >
         <v-card-text>
           <v-container>
             <v-row>
@@ -183,28 +82,6 @@
                   outlined
                 ></v-textarea>
               </v-col>
-              <v-col cols="12">
-                <!-- 이미지 파일이 있을 경우 이미지 미리보기를 표시 -->
-                <v-img
-                  v-if="
-                    detailsNotice.img && detailsNotice.img.startsWith('http')
-                  "
-                  :src="detailsNotice.img"
-                  alt="첨부 이미지"
-                  height="200"
-                ></v-img>
-                <!-- 파일 이름을 표시 -->
-                <v-text-field
-                  v-if="
-                    detailsNotice.img && !detailsNotice.img.startsWith('http')
-                  "
-                  v-model="detailsNotice.img"
-                  label="첨부 파일"
-                  readonly
-                  prepend-icon="mdi-paperclip"
-                  outlined
-                ></v-text-field>
-              </v-col>
             </v-row>
           </v-container>
         </v-card-text>
@@ -228,28 +105,15 @@ export default {
         { text: "공지사항 유형", value: "category" },
         { text: "공지 제목", value: "title" },
         { text: "게시 날짜", value: "postDate" },
-        { text: "수정 삭제", value: "postDate" },
       ],
       notices: [],
-      currentNotice: {
-        category: "",
-        title: "",
-        contents: "",
-        filePath: null,
-        postDate: null,
-      },
       detailsNotice: {
         category: "",
         title: "",
         contents: "",
-        img: "",
         postDate: "",
       },
-      editingNotice: null,
-      noticeCategorys: ["공지", "이벤트"],
-      showCreateNoticeModal: false,
       showDetailsModal: false,
-      imageUrl: null, // 이미지 미리보기 URL
     };
   },
   mounted() {
@@ -265,212 +129,19 @@ export default {
         .catch((error) => {
           console.error("Error fetching the notices:", error);
           alert(
-            `공지사항 생성 실패: ${error.response.status} ${error.response.statusText}`
+            `공지사항 가져오기 실패: ${error.response.status} ${error.response.statusText}`
           );
         });
-    },
-    openModal() {
-      this.currentNotice = {
-        category: "",
-        title: "",
-        contents: "",
-        filePath: null,
-        postDate: null,
-      };
-      this.editingNotice = null;
-      this.showCreateNoticeModal = true;
-      this.imageUrl = null; // 모달을 열 때 이미지 URL을 초기화
-    },
-
-    // 모달 창 닫기 및 데이터 초기화
-    closeModal() {
-      this.showCreateNoticeModal = false;
-      this.editingNotice = null;
-      this.currentNotice = {
-        category: "",
-        title: "",
-        contents: "",
-        filePath: null,
-        postDate: null,
-      };
-      this.imageUrl = null; // 이미지 URL 초기화
-    },
-
-    // 공지사항 생성하기
-    async createNotice() {
-      const formData = new FormData();
-      const noticeData = {
-        ...this.currentNotice,
-        postDate: new Date().toISOString().split("T")[0], // 현재 날짜를 설정
-      };
-      formData.append(
-        "inform",
-        new Blob([JSON.stringify(noticeData)], {
-          type: "application/json",
-        })
-      );
-      if (this.currentNotice.filePath) {
-        const resizedFile = await this.resizeImage(this.currentNotice.filePath);
-        formData.append("file", resizedFile, this.currentNotice.filePath.name);
-      }
-
-      axios
-        .post("http://localhost:8000/informs/", formData, {
-          headers: {
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => {
-          this.notices.push(response.data);
-          this.closeModal();
-          alert("공지사항이 성공적으로 생성되었습니다.");
-        })
-        .catch((error) => {
-          console.error("Error creating the notice:", error);
-          alert("공지사항 생성 실패: " + error.message);
-        });
-    },
-
-    // 공지사항 수정하기
-    async updateNotice() {
-      const formData = new FormData();
-      const noticeData = {
-        ...this.currentNotice,
-        postDate:
-          this.currentNotice.postDate || new Date().toISOString().split("T")[0], // 현재 날짜를 설정
-      };
-      formData.append(
-        "inform",
-        new Blob([JSON.stringify(noticeData)], {
-          type: "application/json",
-        })
-      );
-      if (this.currentNotice.filePath) {
-        const resizedFile = await this.resizeImage(this.currentNotice.filePath);
-        formData.append("file", resizedFile, this.currentNotice.filePath.name);
-      }
-
-      axios
-        .put(
-          `http://localhost:8000/informs/${this.editingNotice.id}`,
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
-          }
-        )
-        .then((response) => {
-          const index = this.notices.findIndex(
-            (n) => n.id === this.editingNotice.id
-          );
-          if (index !== -1) {
-            this.$set(this.notices, index, response.data); // Vue.set 사용
-          }
-          this.closeModal();
-          alert("공지사항이 성공적으로 업데이트되었습니다.");
-        })
-        .catch((error) => {
-          console.error("Error updating the notice:", error);
-          alert("공지사항 업데이트 실패: " + error.message);
-        });
-    },
-
-    // 공지사항 삭제하기
-    deleteNotice(id) {
-      if (confirm("진짜 삭제하시겠습니까?")) {
-        // 사용자에게 삭제 확인 요청
-        axios
-          .delete(`http://localhost:8000/informs/${id}`)
-          .then(() => {
-            this.notices = this.notices.filter((notice) => notice.id !== id); // 성공적으로 삭제된 경우 클라이언트에서도 제거
-            alert("공지사항이 성공적으로 삭제되었습니다."); // 성공 알림
-          })
-          .catch((error) => {
-            console.error("Error deleting the notice:", error);
-            alert("공지사항 삭제 실패: " + error.message); // 실패 알림
-          });
-      } else {
-        console.log("삭제 취소됩니다."); // 사용자가 삭제를 취소한 경우
-      }
-    },
-
-    onFileChanged() {
-      const filePath = this.currentNotice.filePath; // v-model을 통해 연결된 파일 객체 직접 사용
-      if (filePath && filePath.type.startsWith("image/")) {
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          this.imageUrl = e.target.result; // 이미지 URL을 데이터 속성에 저장
-        };
-        reader.readAsDataURL(filePath); // 파일 내용을 읽어 데이터 URL로 변환합니다.
-      } else {
-        this.imageUrl = null; // 선택된 파일이 이미지가 아닐 경우 URL을 null로 설정
-      }
     },
 
     showDetails(item) {
       this.detailsNotice = { ...item };
       this.showDetailsModal = true;
     },
-
-    editNotice(item) {
-      this.currentNotice = { ...item };
-      this.editingNotice = item;
-      this.showCreateNoticeModal = true;
-      this.imageUrl = item.img || null;
-    },
-
-    // 이미지 리사이징 함수
-    resizeImage(file) {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onload = (event) => {
-          const img = new Image();
-          img.src = event.target.result;
-          img.onload = () => {
-            const canvas = document.createElement("canvas");
-            const maxWidth = 800; // 최대 너비 설정
-            const maxHeight = 800; // 최대 높이 설정
-            let width = img.width;
-            let height = img.height;
-
-            // 이미지 비율을 유지하면서 리사이징
-            if (width > height) {
-              if (width > maxWidth) {
-                height = Math.round((height * maxWidth) / width);
-                width = maxWidth;
-              }
-            } else {
-              if (height > maxHeight) {
-                width = Math.round((width * maxHeight) / height);
-                height = maxHeight;
-              }
-            }
-
-            // 캔버스 크기를 조정하고 이미지를 그립니다.
-            canvas.width = width;
-            canvas.height = height;
-            const ctx = canvas.getContext("2d");
-            ctx.drawImage(img, 0, 0, width, height);
-
-            // 리사이징된 이미지를 Blob 형태로 변환
-            canvas.toBlob(
-              (blob) => {
-                resolve(blob);
-              },
-              "image/jpeg",
-              0.8 // 품질을 0.8로 설정하여 용량 줄이기
-            );
-          };
-        };
-        reader.onerror = (error) => reject(error);
-        reader.readAsDataURL(file);
-      });
-    },
   },
 };
 </script>
 
 <style scoped>
-/* 스타일은 컴포넌트의 UI와 레이아웃을 개선하기 위해 추가할 수 있습니다. */
+/* 여기에 스타일을 추가할 수 있습니다. */
 </style>
