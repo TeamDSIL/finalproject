a7be1f817e6b0b8c9db38d4d6062d39cb4d55cd7
LEEDAHYE0513 <ssgdlekgp6801@gmail.com>
2024-05-26 14:41:43 +0900

    #193 Feat: 관리자 화면에서 공지사항 CRUD 추가

--- components/Header.vue
+++ components/Header.vue
@@ -4,31 +4,58 @@
       <v-container class="d-flex align-center">
         <nuxt-link to="/">
           <div class="me-10" tile>
-            <img src="~/assets/images/DSILnewLOGO.png" alt="" style="width: 100px" />
+            <img
+              src="~/assets/images/DSILnewLOGO.png"
+              alt=""
+              style="width: 100px"
+            />
           </div>
         </nuxt-link>
 
         <v-dialog v-model="dsilModal" width="600">
           <v-card>
             <v-card-title>
-              <span style="font-size: large; font-weight: bold; margin-top: 10px;">빠른 검색</span>
+              <span
+                style="font-size: large; font-weight: bold; margin-top: 10px"
+                >빠른 검색</span
+              >
             </v-card-title>
             <v-card-text>
-              <div v-for="category in categories" :key="category.name" class="my-3">
-                <div style="margin-bottom: 10px; display: flex;"><v-img contain
-                    :src="require(`~/assets/images/clikCategory/${category.img}.png`)" width="20px"
-                    style="max-width: 20px; margin-right: 5px"></v-img><span
-                    style="font-weight: bold; font-size: large; color: black;">{{ category.displayName }}</span></div>
+              <div
+                v-for="category in categories"
+                :key="category.name"
+                class="my-3"
+              >
+                <div style="margin-bottom: 10px; display: flex">
+                  <v-img
+                    contain
+                    :src="
+                      require(`~/assets/images/clikCategory/${category.img}.png`)
+                    "
+                    width="20px"
+                    style="max-width: 20px; margin-right: 5px"
+                  ></v-img
+                  ><span
+                    style="font-weight: bold; font-size: large; color: black"
+                    >{{ category.displayName }}</span
+                  >
+                </div>
 
                 <div class="button-grid">
-                  <v-btn v-for="item in category.items" :key="item.name" :class="{
-                    'btn-selected': category.selected.some(
-                      (selected) => selected.name === item.name
-                    ),
-                    'btn-unselected': !category.selected.some(
-                      (selected) => selected.name === item.name
-                    ),
-                  }" text @click="toggleSelection(category.selected, item)">
+                  <v-btn
+                    v-for="item in category.items"
+                    :key="item.name"
+                    :class="{
+                      'btn-selected': category.selected.some(
+                        (selected) => selected.name === item.name
+                      ),
+                      'btn-unselected': !category.selected.some(
+                        (selected) => selected.name === item.name
+                      ),
+                    }"
+                    text
+                    @click="toggleSelection(category.selected, item)"
+                  >
                     {{ item.displayName }}
                   </v-btn>
                 </div>
@@ -42,20 +69,40 @@
           </v-card>
         </v-dialog>
 
-
-        <div class="search-bar-container flex-grow-1 d-none d-md-flex border rounded">
-          <v-btn icon tile large text class="text-capitalize rounded-0" width="120px"
-            style="background-color: rgb(210, 63, 87);" @click="dsilModal = true">
+        <div
+          class="search-bar-container flex-grow-1 d-none d-md-flex border rounded"
+        >
+          <v-btn
+            icon
+            tile
+            large
+            text
+            class="text-capitalize rounded-0"
+            width="120px"
+            style="background-color: rgb(210, 63, 87)"
+            @click="dsilModal = true"
+          >
             <!-- <v-icon small>mdi-crosshairs-gps</v-icon> -->
-            <span style="color: white; font-weight: bold;">빠른검색하기</span>
+            <span style="color: white; font-weight: bold">빠른검색하기</span>
           </v-btn>
-          <input class="white flex-grow-1 ps-5" type="text" placeholder="카테고리 및 음식이름을 입력해주세요" v-model="searchQuery"
-            @keyup.enter="search" />
-          <v-btn icon large tile class="rounded-l-0 text-capitalize" @click="search" width="100px">Search
+          <input
+            class="white flex-grow-1 ps-5"
+            type="text"
+            placeholder="카테고리 및 음식이름을 입력해주세요"
+            v-model="searchQuery"
+            @keyup.enter="search"
+          />
+          <v-btn
+            icon
+            large
+            tile
+            class="rounded-l-0 text-capitalize"
+            @click="search"
+            width="100px"
+            >Search
           </v-btn>
         </div>
         <v-spacer></v-spacer>
-
 
         <!-- <v-btn light text href="http://localhost:3000/memberManage/loginPage">
           <v-icon class="me-0 me-sm-3">mdi-account-circle-outline</v-icon>
@@ -65,9 +112,13 @@
           <v-btn light text @click="logout">
             <span class="d-none d-sm-block">로그아웃</span>
           </v-btn>
-          <v-btn light text href="http://localhost:3000/memberManage/userMyPage">
+          <v-btn
+            light
+            text
+            href="http://localhost:3000/memberManage/userMyPage"
+          >
             <v-icon class="me-0 me-sm-3">mdi-account-circle-outline</v-icon>
-            <span class="d-none d-sm-block">{{ user ? user.email : '' }}</span>
+            <span class="d-none d-sm-block">{{ user ? user.email : "" }}</span>
           </v-btn>
         </template>
 
@@ -80,7 +131,11 @@
         <!-- <v-btn @click="shoppingCartDrawer = true" light text tile class="me-2">
                 <v-icon small>mdi-cart-outline</v-icon>(8)
               </v-btn> -->
-        <v-app-bar-nav-icon text light @click="drawer = true"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+          text
+          light
+          @click="drawer = true"
+        ></v-app-bar-nav-icon>
       </v-container>
     </v-app-bar>
     <!-- <v-navigation-drawer
@@ -124,7 +179,7 @@
   </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   data: () => ({
@@ -247,109 +302,114 @@ export default {
   methods: {
     async fetchUserInfo() {
       try {
-        const token = localStorage.getItem('token'); // 저장된 토큰 가져오기
+        const token = localStorage.getItem("token"); // 저장된 토큰 가져오기
         if (!token) {
-          throw new Error('No token found');
+          throw new Error("No token found");
         }
 
         // 토큰을 Authorization 헤더에 포함하여 요청 보내기
-        const response = await axios.get('http://localhost:8000/userInfo/me', {
+        const response = await axios.get("http://localhost:8000/userInfo/me", {
           headers: {
-            'Authorization': `${token}`
+            Authorization: `${token}`,
           },
-          withCredentials: true
+          withCredentials: true,
         });
 
         if (response.status === 200) {
           const userInfo = response.data;
-          console.log('User Info:', userInfo);
+          console.log("User Info:", userInfo);
           // 사용자 정보를 상태나 컴포넌트 데이터에 저장
           this.user = userInfo;
           console.log(this.user);
           console.log(this.user.id);
         } else {
-          console.error('Failed to fetch user info:', response);
+          console.error("Failed to fetch user info:", response);
         }
       } catch (error) {
-        console.error('Error fetching user info:', error);
+        console.error("Error fetching user info:", error);
       }
     },
     async logout() {
       try {
-        console.log('Sending logout request...');
-        const response = await axios.post('http://localhost:8000/memberManage/logout', {}, { withCredentials: true });
+        console.log("Sending logout request...");
+        const response = await axios.post(
+          "http://localhost:8000/memberManage/logout",
+          {},
+          { withCredentials: true }
+        );
         if (response.status === 200) {
-          console.log('Logout successful');
-          localStorage.removeItem('token');
+          console.log("Logout successful");
+          localStorage.removeItem("token");
           this.user = null;
-          this.$router.push('/memberManage/loginPage');
+          this.$router.push("/memberManage/loginPage");
         } else {
-          console.error('Failed to logout:', response);
+          console.error("Failed to logout:", response);
         }
       } catch (error) {
-        console.error('Error logging out:', error);
+        console.error("Error logging out:", error);
       }
-    }
-  },
-  toggleNavClass() {
-    return this.drawer ? "sticky-nav" : "nav";
-  },
-  toggleSelection(selectedArray, item) {
-    const index = selectedArray.findIndex(
-      (selected) => selected.name === item.name
-    );
-    if (index > -1) {
-      selectedArray.splice(index, 1);
-    } else {
-      selectedArray.push(item);
-    }
-  },
-  closeModal() {
-    this.dsilModal = false;
-    this.categories.forEach((category) => {
-      category.selected = [];
-    });
-  },
-  search() {
-    // 쿼리 파라미터를 담을 객체를 생성합니다.
-    let queryParams = {};
-
-    // 입력된 검색어 처리
-    if (this.searchQuery.trim() !== "") {
-      queryParams["search"] = [encodeURIComponent(this.searchQuery.trim())]; // 배열로 저장
-    }
-
-    // 카테고리별로 쿼리 파라미터를 생성합니다.
-    this.categories.forEach((category) => {
-      category.selected.forEach((item) => {
-        // 시설 관련 카테고리는 'facility' 파라미터로, 나머지는 'category' 파라미터로 추가합니다.
-        const paramKey =
-          category.name === "FACILITIES" ? "facility" : "category";
-        // 해당 키에 대한 배열이 없으면 생성합니다.
-        if (!queryParams[paramKey]) {
-          queryParams[paramKey] = [];
-        }
-        // 해당 카테고리 키 배열에 아이템 이름을 추가합니다.
-        queryParams[paramKey].push(item.name);
+    },
+    toggleNavClass() {
+      return this.drawer ? "sticky-nav" : "nav";
+    },
+    toggleSelection(selectedArray, item) {
+      const index = selectedArray.findIndex(
+        (selected) => selected.name === item.name
+      );
+      if (index > -1) {
+        selectedArray.splice(index, 1);
+      } else {
+        selectedArray.push(item);
+      }
+    },
+    closeModal() {
+      this.dsilModal = false;
+      this.categories.forEach((category) => {
+        category.selected = [];
       });
-    });
+    },
+    search() {
+      // 쿼리 파라미터를 담을 객체를 생성합니다.
+      let queryParams = {};
 
-    // 쿼리 파라미터 문자열로 변환합니다.
-    let queryString = Object.keys(queryParams)
-      .map(
-        (key) => queryParams[key].map((value) => `${key}=${value}`).join("&") // 배열을 이용해 join 함수 호출
-      )
-      .join("&");
+      // 입력된 검색어 처리
+      if (this.searchQuery.trim() !== "") {
+        queryParams["search"] = [encodeURIComponent(this.searchQuery.trim())]; // 배열로 저장
+      }
 
-    // 생성된 쿼리 파라미터를 포함하는 URL을 생성합니다.
-    const searchUrl = `http://localhost:3000/restaurant/list?${queryString}`;
+      // 카테고리별로 쿼리 파라미터를 생성합니다.
+      this.categories.forEach((category) => {
+        category.selected.forEach((item) => {
+          // 시설 관련 카테고리는 'facility' 파라미터로, 나머지는 'category' 파라미터로 추가합니다.
+          const paramKey =
+            category.name === "FACILITIES" ? "facility" : "category";
+          // 해당 키에 대한 배열이 없으면 생성합니다.
+          if (!queryParams[paramKey]) {
+            queryParams[paramKey] = [];
+          }
+          // 해당 카테고리 키 배열에 아이템 이름을 추가합니다.
+          queryParams[paramKey].push(item.name);
+        });
+      });
 
-    // URL로 리디렉션합니다.
-    window.location.href = searchUrl;
+      // 쿼리 파라미터 문자열로 변환합니다.
+      let queryString = Object.keys(queryParams)
+        .map(
+          (key) => queryParams[key].map((value) => `${key}=${value}`).join("&") // 배열을 이용해 join 함수 호출
+        )
+        .join("&");
 
-    // 모달 창을 닫습니다.
-    this.closeModal();
+      // 생성된 쿼리 파라미터를 포함하는 URL을 생성합니다.
+      const searchUrl = `http://localhost:3000/restaurant/list?${queryString}`;
+
+      // URL로 리디렉션합니다.
+      window.location.href = searchUrl;
+
+      // 모달 창을 닫습니다.
+      this.closeModal();
+    },
   },
+
   async mounted() {
     await this.fetchUserInfo();
     window.document.onscroll = () => {
